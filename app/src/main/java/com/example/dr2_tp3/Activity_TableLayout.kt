ed05106a6c18e4352361fd6e9221dadc67bd8750
Cammarota <marcos.cammarota@al.infnet.edu.br>
2022-06-06 17:32:08 -0300

    Apresentação TP3

---
+++ app/src/main/java/com/example/dr2_tp3/Activity_TableLayout.kt
@@ -0,0 +1,11 @@
+package com.example.dr2_tp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_TableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
